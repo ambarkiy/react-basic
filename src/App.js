38aa1d8a72456c5e562547300db24cd5bd6c5fe9
ambarkiy <ambarki.youssef@gmail.com>
2019-12-05 08:08:12 +0100

    react component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const toDo = [
+	"utiliser le state pour mettre un jour les propriétés d'un composant",
+	'on verra pour la suites'
+];
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { toDo: toDo };
+	}
+
+	render() {
+		return this.state.toDo.map((todo) => <h2>{todo}</h2>);
+	}
 }
 
 export default App;
